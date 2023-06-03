25d1e6d035f1df082eca29c99feba925ae532abb
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-04 00:20:35 +0600

    data fetched

--- lib/getAllUsers.tsx
+++ lib/getAllUsers.tsx
@@ -1,6 +1,6 @@
 export default async function getAllUsers() {
     const res = await fetch('https://jsonplaceholder.typicode.com/users');
     if (!res.ok) throw new Error('Error');
-    return await res.json();
+    return res.json();
 }
 
--- types.d.ts
+++ types.d.ts
@@ -1,4 +1,4 @@
-type User = {
+export type User = {
   id: 1;
   name: "Leanne Graham";
   username: "Bret";
