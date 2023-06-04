304beafbba4b17e3c132a4ad4fb78f7806d8eefe
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-04 11:57:29 +0600

    getUser

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -1,5 +1,10 @@
+type Params = {
+    params: {
+        userId: string
+    }
+}
 
-export default function UserPage() {
+export default function UserPage({ params: { userId } }: Params) {
     return (
         <div>page</div>
     )
