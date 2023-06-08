610b227bb2fe9a52b587888c1f3887868b64a119
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-09 01:02:46 +0600

    generateStaticParams

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -1,3 +1,4 @@
+import getAllUsers from "@/lib/getAllUsers"
 import getUser from "@/lib/getUser"
 import getUserPosts from "@/lib/getUserPosts"
 import type { Metadata } from 'next'
@@ -40,4 +41,14 @@ export default async function UserPage({ params: { userId } }: Params) {
             </Suspense>
         </>
     )
+}
+
+
+export async function generateStaticParams() {
+    const usersData: Promise<User[]> = getAllUsers();
+    const users = await usersData;
+
+    return users.map(user => (
+        { userId: `${user.id}` }
+    ))
 }
