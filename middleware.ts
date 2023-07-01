0eab4827f3a70909daedb0068e14078b6d6a5b59
shourovahmd <moh.abuhanifa@gmail.com>
2023-07-02 00:10:01 +0600

    AuthSessionProvider

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,23 @@
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
 
-export function middleware(req: Request) {
-  console.log(req.url);
+// export function middleware(req: Request) {
+//   console.log(req.url);
 
-  if (req.url.includes("/about")) {
-    return NextResponse.redirect(new URL("/", req.url));
-  }
+//   if (req.url.includes("/about")) {
+//     return NextResponse.redirect(new URL("/", req.url));
+//   }
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
 
-// export const config = {
-//   matcher: ["/api/:path*"],
-// };
+// // export const config = {
+// //   matcher: ["/api/:path*"],
+// // };
+
+// Without a defined matcher, this one line applies next-auth
+// to the entire project
+export { default } from "next-auth/middleware";
+
+// Applies next-auth only to matching routes - can be regex
+// Ref: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
+export const config = { matcher: "/dashboard" };
